7413392af58b9c6903a894035e7aefa6415f1fb3
Harini Rajan <145862041+Harinirajan25@users.noreply.github.com>
2024-12-09 18:09:14 +0530

    Add files via upload

---
+++ task4-Dynamic Background Color Changer/script.js
@@ -0,0 +1,27 @@
+let s = document.getElementById("select");
+
+s.style.margin = "50px  650px";
+s.style.padding = "10px 20px";
+s.style.color = "white"
+s.style.backgroundColor = "black"
+s.style.borderRadius
+ = "15px"
+s.style.fontSize = "15px"
+
+
+
+
+
+
+function fun() {
+
+
+    let s1 = document.getElementById("select").value;
+
+    document.body.style.backgroundColor = s1;
+
+
+
+
+
+}
